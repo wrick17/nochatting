7f0d655aa9a20b023cce3c92e5c805efe43a3f70
Pratyush Poddar <pratyush.poddar@compassitesinc.com>
2016-02-04 00:40:08 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,22 @@
+(function() {
+  $('#name-form input').focus();
+  $('#name-form').submit(function(e){
+    e.preventDefault();
+    $(this).hide();
+    var name = $('#name').val();
+    var socket = io();
+    $('#m').focus();
+    $('#chat-form').submit(function(e){
+      e.preventDefault();
+      socket.emit('chat message', {
+        name: name,
+        message: $('#m').val()
+      });
+      $('#m').val('').focus();
+      return false;
+    });
+    socket.on('chat message', function(msg){
+      $('#messages').append($('<li>').html('<label>'+msg.name+':</label> '+msg.message));
+    });
+  });
+})();
